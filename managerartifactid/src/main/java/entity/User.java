d5e2fc7a039a514e78d947178338ba916c33a126
Mars <maxence.tissot@live.fr>
2017-04-24 20:55:23 +0200

    test commit

--- managerartifactid/src/main/java/entity/User.java
+++ managerartifactid/src/main/java/entity/User.java
@@ -1,5 +1,20 @@
 package entity;
 
 public class User {
-
+	private String name;
+	private String firstname;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	
 }
--- managerartifactid/src/main/java/managerartifactid/Main.java
+++ managerartifactid/src/main/java/managerartifactid/Main.java
@@ -1,10 +1,16 @@
 package managerartifactid;
 
+import entity.User;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		User user1 = new User();
+		user1.setName("toto");
+		user1.setFirstname("titi");
+		
+				
 	}
 
 }
