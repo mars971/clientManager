d13d43b87d4d1f63e67d6172864625f7b864e90e
Mars <maxence.tissot@live.fr>
2017-12-10 23:38:30 +0100

    update with IntelliJ

---
+++ managerartifactid/src/main/java/entity/Adresse.java
@@ -0,0 +1,12 @@
+package entity;
+
+public class Adresse {
+    private String cp;
+    private String ville;
+    private String Departement;
+    private String region;
+    private String route;
+    private String immeuble;
+    private String voie;
+
+}
