989c28cb3d99512cac5a7b05ea71bb676d6d3155
Mars <maxence.tissot@live.fr>
2018-01-04 00:03:26 +0100

    update at home

--- entityManager/src/main/java/Person/Person.java
+++ entityManager/src/main/java/Person/Person.java
@@ -1,6 +1,12 @@
 package Person;
 
+import Adress.Adress;
+import Observation.Observation;
+
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
 
 @Entity
