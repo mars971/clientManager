d89b95eb0a56044c1e1b5b951550af14ee0c0842
tissot <maxence.tissot@live.fr>
2018-01-15 15:16:57 +0100

    update des couches

--- dao/src/main/java/EAdress/AdressDAO.java
+++ dao/src/main/java/EAdress/AdressDAO.java
@@ -3,16 +3,116 @@ package EAdress;
 import EAddress.Address;
 import ECustomer.Customer;
 import EPerson.Person;
+import org.hibernate.Criteria;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class AdressDAO {
 
-    public Address findById(Integer idAddress){
+    public Address findByCriterion(Criteria criterion){
         Address address = new Address();
+        address.setRoute(criterion.toString() );
 
         return address;
+    }
+
+    public Address findByCriteria(Criteria criteria){
+        Address address = new Address();
+        address.setRoute(criteria.toString() );
+
+        return address;
+    }
+
+    public List<Address> findAdressesByCriterion(Criteria criteria){
+        List<Address>addresses = new ArrayList<Address>();
+
+        Address address1 = new Address();
+        address1.setRoute(criteria.toString() );
+
+        Address address2 = new Address();
+        address2.setRoute(criteria.toString() );
+
+        addresses.add(address1);
+        addresses.add(address2);
+
+        return addresses;
+    }
+
+    public List<Address> findAdressesByCriteria(Criteria criteria){
+        List<Address>addresses = new ArrayList<Address>();
+        addresses.add(findById(1));
+
+        return addresses;
+    }
+
+    public Address findById(Integer idAddress){
+        Address address = new Address();
+        address.setId(idAddress);
+
+        return address;
+    }
+
+    public List<Address> findByCP(String cp){
+        List<Address>addresses = new ArrayList<Address>();
+        addresses.add(findById(1));
+
+        return addresses;
+    }
+
+    public List<Address> findbyVille(String ville){
+        List<Address>addresses = new ArrayList<Address>();
+        addresses.add(findById(1));
+
+        return addresses;
+    }
+
+    public List<Address> findByDepartement(String department){
+        List<Address>addresses = new ArrayList<Address>();
+        addresses.add(findById(1));
+
+        return addresses;
+    }
+
+    public List<Address> findByRegion(String region){
+        List<Address>addresses = new ArrayList<Address>();
+        addresses.add(findById(1));
+
+        return addresses;
+    }
+
+    public List<Address> findByRoute(String route){
+        List<Address>addresses = new ArrayList<Address>();
+        addresses.add(findById(1));
+
+        return addresses;
+    }
+
+    private String voie;
+
+    public List<Address> findByImmeuble(String immeuble){
+        List<Address>addresses = new ArrayList<Address>();
+        addresses.add(findById(1));
+
+        return addresses;
+    }
+
+    public List<Address> findByVoie(String voie){
+        List<Address>addresses = new ArrayList<Address>();
+        addresses.add(findById(1));
+
+        return addresses;
+    }
+
+    public List<Address> findByCustomer(Customer customer){
+        Address addressHome = new Address();
+        Address addressWork = new Address();
+
+        List<Address>addresses = new ArrayList<Address>();
+        addresses.add(addressHome);
+        addresses.add(addressWork);
+
+        return addresses;
     }
 
     public List<Address> findByPersonn(Person personn){
@@ -44,22 +144,10 @@ public class AdressDAO {
         return addresses;
     }
 
-    public List<Address> findByCustomer(Customer customer){
-        Address addressHome = new Address();
-        Address addressWork = new Address();
-
-        List<Address>addresses = new ArrayList<Address>();
-        addresses.add(addressHome);
-        addresses.add(addressWork);
-
-        return addresses;
-    }
-
     public List<Address> findAll(){
         List<Address>addresses = new ArrayList<Address>();
 
         return addresses;
     }
-
 
 }
--- service/src/main/java/address/AddressService.java
+++ service/src/main/java/address/AddressService.java
@@ -3,6 +3,7 @@ package address;
 import EAdress.AdressDAO;
 import ECustomer.Customer;
 import EPerson.Person;
+import org.hibernate.Criteria;
 
 import java.util.List;
 
@@ -10,9 +11,46 @@ public class AddressService {
 
     private AdressDAO adressDAO;
 
+    public EAddress.Address getAddressByCriterion(Criteria criterion){
+        return adressDAO.findByCriterion(criterion);
+    }
+
+    public EAddress.Address getAddressByCriteria(Criteria criteria){
+        return adressDAO.findByCriteria(criteria);
+    }
+
+
+    public List<EAddress.Address> getAddressesByCriterion(Criteria Criterion){
+        return adressDAO.findAdressesByCriterion(Criterion);
+    }
+
+    public List<EAddress.Address> getAddressesByCriteria(Criteria criteria){
+        return adressDAO.findAdressesByCriteria(criteria);
+    }
+
     public EAddress.Address getAddressById(Integer idAddress){
         return adressDAO.findById(idAddress);
     }
+    public List<EAddress.Address> getAddressByCP(String cp){
+        return adressDAO.findByCP(cp);
+    }
+    public List<EAddress.Address> getAddressByVille(String ville){
+        return adressDAO.findbyVille(ville);
+    }
+
+    public List<EAddress.Address> getAddressByDepartment(String department){
+        return adressDAO.findByDepartement(department);
+    }
+
+    public List<EAddress.Address> getAddressByRegion(String region){
+        return adressDAO.findByRegion(region);
+    }
+
+    public List<EAddress.Address> getAddressByRoute(String route){ return adressDAO.findByRoute(route); }
+    public List<EAddress.Address> getAddressByImmeuble(String immeuble){
+        return adressDAO.findByImmeuble(immeuble);
+    }
+    public List<EAddress.Address> getAddressByVoie(String voie){      return adressDAO.findByVoie(voie);}
 
     public List<EAddress.Address> getAddressByPersonn(Person person){
         return adressDAO.findByPersonn(person);
